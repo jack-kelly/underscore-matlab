41984cc39a1965265542b5228bc8a8114dba8fe6
Jack Kelly <jpkelly@mtu.edu>
2015-04-27 15:02:02 -0400

    Initial fold(l/r) functions.

--- collections/foldl.m
+++ collections/foldl.m
@@ -1,3 +1,17 @@
-function out = foldl(fun, collection)
+function out = foldl(fun, collection, x0)
+	if nargin <3 || isempty(x0)
+		out = foldr(fun,initial(collection),last(collection));
+		return
+	end
 
+	out = x0;
+	if iscell(collection)
+		for k = numel(collection):-1:1
+			out = fun(out, collection{k});
+		end
+	else
+		for k = numel(collection):-1:1
+			out = fun(out, collection(k));
+		end
+	end
 end
--- collections/foldr.m
+++ collections/foldr.m
@@ -1,3 +1,17 @@
-function out = foldr(fun, collection)
+function out = foldr(fun, collection, x0)
+	if nargin <3 || isempty(x0)
+		out = foldr(fun,tail(collection),head(collection));
+		return
+	end
 
+	out = x0;
+	if iscell(collection)
+		for k = 1:numel(collection)
+			out = fun(out, collection{k});
+		end
+	else
+		for k = 1:numel(collection)
+			out = fun(out, collection(k));
+		end
+	end
 end
