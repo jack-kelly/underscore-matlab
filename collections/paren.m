ef338bf75fe251cdbfde21ae8fde25c7e68089c1
Jack Kelly <jpkelly@mtu.edu>
2015-04-28 16:04:11 -0400

    Paren can operate on functions, updated help to show that.

--- collections/paren.m
+++ collections/paren.m
@@ -1,10 +1,10 @@
 % out = paren(cell_array,inds1,inds2,...,indsN)
 %
-% Functional interface to array slicing
+% Functional interface to array slicing / function evaluation
 %
 % For example, A(1,:,1:3) is equivalent to paren(A,1,':',1:3)
 %
 % Note: pass the string ':' for :
-function out = paren(collection, varargin)
-	out = collection(varargin{:});
+function out = paren(thing, varargin)
+	out = thing(varargin{:});
 end
